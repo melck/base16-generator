b770fc63ac147e95323bd9f1e9eeb2b1438fd05c
Melchior NOGUES <mnogues@ecritel.net>
2021-09-16 12:12:41 +0200

    fix: Fix #15
    - Add setting base16.generator.activatedThemes
    - Fix commands to use setting
    - Add check setting at start and activate themes if needed

--- src/extension.ts
+++ src/extension.ts
@@ -1,203 +1,100 @@
-'use strict';
-import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
-var cjson = require('strip-json-comments');
+"use strict";
+import * as vscode from "vscode";
+import { getThemeItems, promptRestart, readExistingThemes, writeActivatedThemes } from "./utils";
 
-export function activate(context: vscode.ExtensionContext) {
-    let activateThemeCommand = vscode.commands.registerCommand('base16.generator.activateTheme', function () {
-        activateTheme();
-    });
-    let deactivateThemeCommand = vscode.commands.registerCommand('base16.generator.deactivateTheme', function() {
-        deactivateTheme();
-    });
-    let activateAllThemesCommand = vscode.commands.registerCommand('base16.generator.activateAllThemes', function() {
-        activateAllThemes();
-    });
-    let deactivateAllThemesCommand = vscode.commands.registerCommand('base16.generator.deactivateAllThemes', function() {
-        deactivateAllThemes();
-    });
+export function activate(context: vscode.ExtensionContext): void {
+  const disposables: vscode.Disposable[] = [
+    vscode.commands.registerCommand("base16.generator.activateTheme", () => activateTheme()),
+    vscode.commands.registerCommand("base16.generator.deactivateTheme", () => deactivateTheme()),
+    vscode.commands.registerCommand("base16.generator.activateAllThemes", () => activateAllThemes()),
+    vscode.commands.registerCommand("base16.generator.deactivateAllThemes", () => deactivateAllThemes()),
+    vscode.workspace.onDidChangeConfiguration((e) => onDidChangeConfiguration(e)),
+  ];
 
-    context.subscriptions.push(activateThemeCommand);
-    context.subscriptions.push(deactivateThemeCommand);
-    context.subscriptions.push(activateAllThemesCommand);
-    context.subscriptions.push(deactivateAllThemesCommand);
+  for (const disposable of disposables) {
+    context.subscriptions.push(disposable);
+  }
+
+  // If reinstalling or updating, it will load themes in settings
+  applyChanges();
 }
 
-async function activateTheme(): Promise<void> {
-    const themesDir: string = path.resolve(__dirname, '../../themes');
-    const themes = fs.readdirSync(themesDir);
-    const themesList: vscode.QuickPickItem[] = themes.map(t => {
-        const loadedTheme = parseJson(fs.readFileSync(path.join(themesDir, t), 'utf8'));
-        const item: vscode.QuickPickItem = {
-            label: loadedTheme.name,
-            description: t
-        };
-        return item;
-    });
+async function activateTheme() {
+  let configThemes = vscode.workspace.getConfiguration().get("base16.generator.activatedThemes") as string[];
+  const themeItems = getThemeItems(configThemes);
 
-    const selectedThemes: vscode.QuickPickItem[] = await vscode.window.showQuickPick(themesList, {
-        ignoreFocusOut: false,
-        matchOnDescription: false,
-        matchOnDetail: false,
-        placeHolder: 'Base16 Default Dark',
-        canPickMany: true
-    });
+  const selectedThemes = await vscode.window.showQuickPick(themeItems, {
+    ignoreFocusOut: false,
+    matchOnDescription: false,
+    matchOnDetail: false,
+    placeHolder: "Search a theme",
+    canPickMany: true,
+  });
 
-    if (!selectedThemes) {
-        return;
-    }
+  if (!selectedThemes) {
+    return;
+  }
 
-    const packageInfo = getPackageInfo();
-    let numberOfThemesActivated: number = 0;
-    for (const selectedTheme of selectedThemes) {
-        const themeFile = parseJson(fs.readFileSync(path.join(themesDir, selectedTheme.description), 'utf8'));
-        let themeType: string = 'vs-dark';
-        if (themeFile.type == 'dark') {
-            themeType = 'vs-dark';
-        }
-        else if (themeFile.type == 'light') {
-            themeType = 'vs';
-        }
-
-        const theme = {
-            label: selectedTheme.label,
-            uiTheme: themeType,
-            path: './themes/' + selectedTheme.description
-        };
-        packageInfo.contributes.themes.push(theme);
-        numberOfThemesActivated += 1;
-    }
-    
-    writePackageInfo(packageInfo);
-
-    let restartString = '';
-    if (numberOfThemesActivated == 1) {
-        restartString = `${numberOfThemesActivated} Base16 theme has been activated. Please restart VSCode and then go to Preferences: Color Theme.`;
-    } else {
-        restartString = `${numberOfThemesActivated} Base16 themes have been activated. Please restart VSCode and then go to Preferences: Color Theme.`;
-    }
-
-    await promptRestart(restartString);
+  configThemes = selectedThemes.map((t) => t.description) as string[];
+  await vscode.workspace.getConfiguration().update("base16.generator.activatedThemes", configThemes, true);
 }
 
-async function activateAllThemes(): Promise<void> {
-    const themesDir: string = path.resolve(__dirname, '../../themes');
-    const themes = fs.readdirSync(themesDir);
+async function deactivateTheme() {
+  let configThemes = vscode.workspace.getConfiguration().get("base16.generator.activatedThemes") as string[];
+  const themeItems = getThemeItems(configThemes, true);
 
-    const packageInfo = getPackageInfo();
-    packageInfo.contributes.themes = [];
+  const selectedThemes = await vscode.window.showQuickPick(themeItems, {
+    ignoreFocusOut: false,
+    matchOnDescription: false,
+    matchOnDetail: false,
+    placeHolder: "Search a theme",
+    canPickMany: true,
+  });
 
-    for (const theme of themes) {
-        const themeFile = parseJson(fs.readFileSync(path.join(themesDir, theme), 'utf8'));
-        let themeType: string = 'vs-dark';
-        if (themeFile.type == 'dark') {
-            themeType = 'vs-dark';
-        } else if (themeFile.type == 'light') {
-            themeType = 'vs';
-        }
+  if (!selectedThemes) {
+    return;
+  }
 
-        const extensionTheme = {
-            label: themeFile.name,
-            uiTheme: themeType,
-            path: './themes/' + theme
-        };
-        packageInfo.contributes.themes.push(extensionTheme);
-    }
+  configThemes = selectedThemes.map((t) => t.description) as string[];
 
-    writePackageInfo(packageInfo);
-
-    await promptRestart('All Base16 themes have been activated. Please restart VSCode and then go to Preferences: Color Theme.');
+  const target = vscode.ConfigurationTarget.Global;
+  await vscode.workspace.getConfiguration().update("base16.generator.activatedThemes", configThemes, target);
 }
 
-async function deactivateTheme(): Promise<void> {
-    const packageInfo = getPackageInfo();
-    if (packageInfo.contributes.themes.length == 0) {
-        vscode.window.showInformationMessage('No themes to deactivate.');
-        return;
-    }
-
-    const activatedThemes: vscode.QuickPickItem[] = packageInfo.contributes.themes.map(theme => {
-        let themePathSplit: string[] = theme.path.split('/');
-        let themeJson: string = themePathSplit[themePathSplit.length - 1];
-
-        let item: vscode.QuickPickItem = {
-            label: theme.label,
-            description: themeJson
-        };
-        return item;
-    });
-
-    const deactivatedThemes: vscode.QuickPickItem[] = await vscode.window.showQuickPick(activatedThemes, {
-        ignoreFocusOut: false,
-        matchOnDescription: false,
-        matchOnDetail: false,
-        canPickMany: true
-    });
-
-    if (!deactivatedThemes) {
-        return;
-    }
-
-    let numberOfThemesDeactivated: number = 0;
-    for (const theme of deactivatedThemes) {
-        for (let i = 0; i < packageInfo.contributes.themes.length; i++) {
-            const packageInfoTheme = packageInfo.contributes.themes[i];
-            if (packageInfoTheme.label == theme.label) {
-                packageInfo.contributes.themes.splice(i, 1);
-                i -= 1;
-                numberOfThemesDeactivated += 1;
-                break;
-            }
-        }
-    }
-
-    writePackageInfo(packageInfo);
-
-    let restartString = '';
-    if (numberOfThemesDeactivated == 1) {
-        restartString = `${numberOfThemesDeactivated} Base16 theme has been deactivated. Please restart VSCode.`;
-    } else {
-        restartString = `${numberOfThemesDeactivated} Base16 themes have been deactivated. Please restart VSCode.`;
-    }
-
-    await promptRestart(restartString);
+async function activateAllThemes() {
+  const target = vscode.ConfigurationTarget.Global;
+  await vscode.workspace.getConfiguration().update("base16.generator.activatedThemes", readExistingThemes(), target);
 }
 
-async function deactivateAllThemes(): Promise<void> {
-    const packageInfo = getPackageInfo();
-    if (packageInfo.contributes.themes.length == 0) {
-        vscode.window.showInformationMessage('No themes to deactivate.');
-        return;
-    }
-
-    packageInfo.contributes.themes = [];
-    writePackageInfo(packageInfo);
-    await promptRestart('All Base16 themes have been deactivated. Please restart VSCode.');
+async function deactivateAllThemes() {
+  const target = vscode.ConfigurationTarget.Global;
+  await vscode.workspace.getConfiguration().update("base16.generator.activatedThemes", [], target);
 }
 
-function getPackageInfo(): any {
-    return parseJson(fs.readFileSync(path.resolve(__dirname, '../../package.json'), 'utf8'));
+async function onDidChangeConfiguration(e: vscode.ConfigurationChangeEvent) {
+  if (e.affectsConfiguration("base16.generator.activatedThemes")) {
+    applyChanges();
+  }
 }
 
-function writePackageInfo(packageInfo: any): void {
-    fs.writeFileSync(path.resolve(__dirname, '../../package.json'), JSON.stringify(packageInfo, null, 2));
-}
+async function applyChanges() {
+  const themes = vscode.workspace.getConfiguration().get("base16.generator.activatedThemes") as string[];
+  const result = writeActivatedThemes(themes);
 
-async function promptRestart(informationMessage: string): Promise<void> {
-    let reloadAction: vscode.MessageItem = {title: 'Reload Now'};
-    let selectedAction = await vscode.window.showInformationMessage(informationMessage, reloadAction);
-    if (!selectedAction) {
-        return;
-    }
-    if (selectedAction.title == reloadAction.title) {
-        await vscode.commands.executeCommand('workbench.action.reloadWindow');
-    }
-}
+  if (result.equal) {
+    return;
+  }
 
-function parseJson(text: string) {
-    return JSON.parse(cjson(text));
-}
+  const resultStrings = [];
+  if (result.added) {
+    resultStrings.push(`${result.added} Added`);
+  }
+  if (result.removed) {
+    resultStrings.push(`${result.removed} Removed`);
+  }
+  if (!result.added && !result.removed && result.reordered) {
+    resultStrings.push(`${result.reordered} Ordered`);
+  }
 
-export function deactivate() {
+  await promptRestart(`Base16 theme has changed (${resultStrings.join(", ")}). Please restart VSCode.`);
 }
